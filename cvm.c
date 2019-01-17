606de13d10fd20789eb7642800880945f08cfa14
Joshua Weinstein <jweinstein@splunk.com>
2019-01-16 17:01:47 -0800

    vm work

--- cvm.c
+++ cvm.c
@@ -33,9 +33,20 @@ static struct Cell* CELL_PTR = CELLS;
 // Boundary pointers
 static const struct Cell* const CELL_BEGIN = CELLS;
 static const struct Cell* const CELL_END = CELLS + CELL_AMNT;
+/* This copies the in field to the out field in order
+ * to prepare for running the operation.
+ * Note: the in field is not preserved if a nested call is present.
+ */
+#define CELL_COPY_IN_OUT(cell) (cell->_out = cell->_in)
 
 /**
  * Byte code instruction labels
+ * uses -----
+ * - Stop: Terminates the running of instructions, aka exit.
+ * - inc ptr: Increments the cell pointer by one. Cannot increment pointer 
+ *            to end cell
+ * - dec ptr: decrements the cell pointer by one. Cannot decrement before begin
+ *            pointer.
  */
 #define CELLINS_X_STOP       0
 #define CELLINS_X_INC_PTR    1
@@ -49,13 +60,10 @@ static const struct Cell* const CELL_END = CELLS + CELL_AMNT;
 #define CELLINS_X_PUSH_NEXT  9
 #define CELLINS_X_CALL_OP    10
 #define CELLINS_X_JUMP       11
-/**
- * Byte Value Type labels
- */
-#define CELLINS_T_STOP 0 // Marks end of value list for op
-#define CELLINS_T_INT  1
-#define CELLINS_T_PLUS 2
-#define CELLINS_T_SUB  3
+#define CELLINS_X_END_OP     12
+#define CELLINS_X_INT        13
+#define CELLINS_X_PLUS       14
+#define CELLINS_x_SUB        15
 
 
 
@@ -80,61 +88,12 @@ cells_core_dump(int depth)
 	}
 }
 
-extern int
+extern unsigned
 cells_run_code(unsigned char* code)
 {
-	int jump_c;
-	unsigned char* code_start = code;
-	while(*code != CELLINS_X_STOP)
-	{
-		switch(*code++)
-		{
-			case CELLINS_X_STOP:
-			   return 1;
-			case CELLINS_X_INC_PTR:
-			   assert((++CELL_PTR) != CELL_END);
-			   break;
-			case CELLINS_X_DEC_PTR:
-			   assert((CELL_PTR--) != CELL_BEGIN);
-			   break;
-			case CELLINS_X_PUT_IN:
-			   CELL_PTR->_in = *(int*)(code);
-			   code += sizeof(int);
-			   break;
-			case CELLINS_X_PUT_OUT:
-			   CELL_PTR->_out = *(int*)(code);
-			   code += sizeof(int);
-			   break;
-			case CELLINS_X_PUT_OP:
-			   CELL_PTR->_op = *(int*)(code);
-			   code += sizeof(int);
-			   break;
-			case CELLINS_X_PRINT_IN:
-			   printf("%d\n", CELL_PTR->_in);
-			   break;
-			case CELLINS_X_PRINT_OUT:
-			   printf("%d\n", CELL_PTR->_out);
-			case CELLINS_X_PUSH_BACK:
-			   assert(CELL_PTR != CELL_BEGIN);
-			   (CELL_PTR - 1)->_in = CELL_PTR->_out;
-			   break;
-			case CELLINS_X_PUSH_NEXT:
-			   assert((CELL_PTR + 1) != CELL_END);
-			   (CELL_PTR + 1)->_in = CELL_PTR->_out;
-			   break;
-			case CELLINS_X_CALL_OP:
-			   // call the op apply function
-			   break;
-			case CELLINS_X_JUMP:
-			   jump_c = (*(int*)(code));
-			   code = code_start + jump_c;
-			   break;
-			default:
-			   fprintf(stderr, "INS ERROR: Unkown instruction code byte %u\n", *code);
-			   cells_core_dump(50);
-			   exit(2);
-		}
-	}
+	 const unsigned char * const code_start = code;
+
+	 return code - code_start;
 }
 
 int main(int argc, char const *argv[])
